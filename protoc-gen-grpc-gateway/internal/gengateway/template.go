3a3719dded5b6ed5ff862e649d9f12a5375ea2f8
rkapka <rkapka@wp.pl>
2021-05-24 10:19:52 +0200

    Revert "Always use params parser"
    This reverts commit 1636751cb567ad740641e521260037c7d70e1f77.

--- protoc-gen-grpc-gateway/internal/gengateway/template.go
+++ protoc-gen-grpc-gateway/internal/gengateway/template.go
@@ -50,7 +50,20 @@ func (b binding) GetBodyFieldStructName() (string, error) {
 // It sometimes returns true even though actually the binding does not need.
 // But it is not serious because it just results in a small amount of extra codes generated.
 func (b binding) HasQueryParam() bool {
-	return true
+	if b.Body != nil && len(b.Body.FieldPath) == 0 {
+		return false
+	}
+	fields := make(map[string]bool)
+	for _, f := range b.Method.RequestType.Fields {
+		fields[f.GetName()] = true
+	}
+	if b.Body != nil {
+		delete(fields, b.Body.FieldPath.String())
+	}
+	for _, p := range b.PathParams {
+		delete(fields, p.FieldPath.String())
+	}
+	return len(fields) > 0
 }
 
 func (b binding) QueryParamFilter() queryParamFilter {
